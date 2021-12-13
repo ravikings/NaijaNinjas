8e4235859fd68b287176a46a5f661d9be9177c6e
ravikings <39674414+ravikings@users.noreply.github.com>
2021-12-12 21:15:44 -0600

    test ignore files

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
+# testing out cache is ignore
 
 class AccountUser(AbstractUser):
     # We don't need to define the email attribute because is inherited from AbstractUser
