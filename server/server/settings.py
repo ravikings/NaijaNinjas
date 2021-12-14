038b21f78d10e7d32aff67334cb13780a1cc7141
ravikings <39674414+ravikings@users.noreply.github.com>
2021-12-13 19:27:33 -0600

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -95,12 +95,17 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1:3000",
-    'http://localhost:8000',
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1:3000",
+#     'http://localhost:8000',
+# )
+
+
+ALLOWED_HOSTS=['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'server.urls'
 
