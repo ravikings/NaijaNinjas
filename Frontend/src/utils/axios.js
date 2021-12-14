af70805b228926d731528240c7502f0ab8d31d18
kasun7153 <kasun7153@gmail.com>
2021-12-14 08:48:15 +0530

    merge

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -4,6 +4,10 @@ export default function createRequest() {
     const idToken = localStorage.getItem('idToken');
     return axios.create({
         baseURL: `http://127.0.0.1:8000/`,
-        withCredentials: true
+        headers:{
+            'Content-Type':'application/json',
+            'Accept': "application/json",
+        },
+        withCredentials: true,
     });
 }
