bdb0e924d8f81bd88036ad54f0066c962fc23f0f
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-05-12 02:37:05 +0500

    banner added!

--- Frontend/src/markup/Element/IndexBanner.js
+++ Frontend/src/markup/Element/IndexBanner.js
@@ -1,9 +1,21 @@
 import React, { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Alert, Form } from "react-bootstrap";
+import { Alert, Card, Form } from "react-bootstrap";
 import { toast } from "react-toastify";
 
 var bnr1 = require("./../../images/main-slider/slide2.jpg");
+
+const style = {
+  margin: "0 auto",
+  position: "absolute",
+  top: "129px",
+  right: "75px",
+  "@media screen and (max-width: 480px)": {
+    position: "relative",
+    top: "129px",
+    right: "75px",
+  },
+};
 
 function IndexBanner() {
   const [title, setTitle] = React.useState("");
@@ -54,73 +66,93 @@ function IndexBanner() {
   }, []);
 
   return (
-    <div
-      className='dez-bnr-inr dez-bnr-inr-md'
-      style={{ backgroundImage: "url(" + bnr1 + ")" }}
-    >
-      <div className='container'>
-        <div className='dez-bnr-inr-entry align-m'>
-          <div className='find-job-bx'>
-            <Link to={"/browse-job"} className='site-button button-sm'>
-              Find Jobs, Employment & Career Opportunities
-            </Link>
-            <h2>
-              Search Between More Then <br />{" "}
-              <span className='text-primary'>50,000</span> Open Jobs.
-            </h2>
-            <form className='dezPlaceAni'>
-              <div className='row'>
-                <div className='col-lg-6 col-md-6'>
-                  <div className='form-group'>
-                    <label>Job Title, Keywords, or Phrase</label>
-                    <div className='input-group'>
-                      <input
-                        type='text'
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        className='form-control'
-                        placeholder=''
-                      />
-                      <div className='input-group-append'>
-                        <span className='input-group-text'>
-                          <i className='fa fa-search'></i>
-                        </span>
+    <>
+      <div
+        className='dez-bnr-inr dez-bnr-inr-md'
+        style={{ backgroundImage: "url(" + bnr1 + ")" }}
+      >
+        <div className='container'>
+          <div className='dez-bnr-inr-entry align-m'>
+            <div className='find-job-bx'>
+              <Link to={"/browse-job"} className='site-button button-sm'>
+                Find Jobs, Employment & Career Opportunities
+              </Link>
+              <h2>
+                Search Between More Then <br />{" "}
+                <span className='text-primary'>50,000</span> Open Jobs.
+              </h2>
+              <form className='dezPlaceAni'>
+                <div className='row'>
+                  <div className='col-lg-6 col-md-6'>
+                    <div className='form-group'>
+                      <label>Job Title, Keywords, or Phrase</label>
+                      <div className='input-group'>
+                        <input
+                          type='text'
+                          value={title}
+                          onChange={(e) => setTitle(e.target.value)}
+                          className='form-control'
+                          placeholder=''
+                        />
+                        <div className='input-group-append'>
+                          <span className='input-group-text'>
+                            <i className='fa fa-search'></i>
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className='col-lg-4 col-md-6'>
-                  <div className='form-group'>
-                    <Form.Control as='select' custom className='select-btn'>
-                      <option>Select Sector</option>
-                      <option>Construction</option>
-                      <option>Corodinator</option>
-                      <option>Employer</option>
-                      <option>Financial Career</option>
-                      <option>Information Technology</option>
-                      <option>Marketing</option>
-                      <option>Quality check</option>
-                      <option>Real Estate</option>
-                      <option>Sales</option>
-                      <option>Supporting</option>
-                      <option>Teaching</option>
-                    </Form.Control>
+                  <div className='col-lg-4 col-md-6'>
+                    <div className='form-group'>
+                      <Form.Control as='select' custom className='select-btn'>
+                        <option>Select Sector</option>
+                        <option>Construction</option>
+                        <option>Corodinator</option>
+                        <option>Employer</option>
+                        <option>Financial Career</option>
+                        <option>Information Technology</option>
+                        <option>Marketing</option>
+                        <option>Quality check</option>
+                        <option>Real Estate</option>
+                        <option>Sales</option>
+                        <option>Supporting</option>
+                        <option>Teaching</option>
+                      </Form.Control>
+                    </div>
+                  </div>
+                  <div className='col-lg-2 col-md-6'>
+                    <button
+                      onClick={handleSubmit}
+                      className='site-button btn-block'
+                    >
+                      Find Job
+                    </button>
                   </div>
                 </div>
-                <div className='col-lg-2 col-md-6'>
-                  <button
-                    onClick={handleSubmit}
-                    className='site-button btn-block'
-                  >
-                    Find Job
-                  </button>
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div style={style}>
+        <Card>
+          <Card.Img
+            variant='top'
+            src='https://image-placeholder.com/images/actual-size/75x75.png'
+            style={{ height: "110px" }}
+          />
+          <Card.Body>
+            <Card.Title>Cafe Figma</Card.Title>
+            <Card.Text>
+              lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </Card.Text>
+            <button className='site-button btn-block' variant='primary'>
+              Open
+            </button>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   );
 }
 export default IndexBanner;
