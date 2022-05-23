9b95f61552596ed1180eee2cd9a186c8f654548f
ravikings <39674414+ravikings@users.noreply.github.com>
2022-05-24 00:27:46 +0100

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "ckeditor",
     "django_filters",
     'rest_framework_simplejwt',
+    'django_s3_storage',
     # "hitcount",
     # Local
     "forum",
@@ -302,6 +303,19 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
     }
 }
+
+YOUR_S3_BUCKET = "zappa-wnf4dp8g2"
+
+STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+AWS_S3_BUCKET_NAME_STATIC = YOUR_S3_BUCKET
+
+# These next two lines will serve the static files directly 
+# from the s3 bucket
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % YOUR_S3_BUCKET
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# OR...if you create a fancy custom domain for your static files use:
+#AWS_S3_PUBLIC_URL_STATIC = "https://static.zappaguide.com/"
 
 
 # EMAIL_HOST = 'smtp.gmail.com'
