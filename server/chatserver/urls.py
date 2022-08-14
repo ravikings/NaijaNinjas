ae92ff29bdd9c1a45016bbf68aee4c91f7ebc2c7
ravikings <39674414+ravikings@users.noreply.github.com>
2022-08-14 11:51:25 -0500

    add more data to chat api

--- server/chatserver/urls.py
+++ server/chatserver/urls.py
@@ -3,7 +3,7 @@ from chatserver.views import start_convo, get_conversation, conversations, delet
 
 urlpatterns = [
     path("start/<str:starter>/<str:pk>/", start_convo, name="start_convo"),
-    path("<int:convo_id>/", get_conversation, name="get_conversation"),
+    path("message_room/<int:convo_id>/", get_conversation, name="get_conversation"),
     path("conversations/<str:pk>/", conversations, name="conversations"),
     path("delete-conversations/<str:convo_id>/", delete_conversation, name="delete-conversations"),
 ]
