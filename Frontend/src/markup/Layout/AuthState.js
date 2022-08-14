30eb0e88ba35eab16b42edb4523699ab4a6be8da
ravikings <39674414+ravikings@users.noreply.github.com>
2022-08-13 22:12:09 -0500

    Update AuthState.js

--- Frontend/src/markup/Layout/AuthState.js
+++ Frontend/src/markup/Layout/AuthState.js
@@ -317,8 +317,8 @@ function AuthState({ userDetails }) {
               </Avatar>
             }
           </div>
-          <Link to={"/post-ads"} title="READ MORE" className="site-button">
-            <i className="fas fa-comments-dollar"></i> CORRECT BUSINESS{" "}
+          <Link to={"/post-ads"} title="READ MORE" className="btn btn-warning">
+            <i className="fas fa-treasure-chest"></i> CORRECT BUSINESS{" "}
           </Link>
           {/* 
           <ReactButton
@@ -354,7 +354,7 @@ function AuthState({ userDetails }) {
               title="READ MORE"
               className="btn btn-warning"
             >
-              <i className="fas fa-comments-dollar"></i> CORRECT BUSINESS{" "}
+              <i className="fas fa-treasure-chest"></i> CORRECT BUSINESS{" "}
             </Link>
             {/* <ReactButton
               style={{
