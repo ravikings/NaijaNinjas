65b098cac31a61325a2af92dd7d5788533f256f3
Shahbaazkyz <Shahbaazkyz@gmail.com>
2022-09-03 23:50:03 +0500

    fix no service

--- Frontend/src/markup/Pages/MakeOffer/components/Services.js
+++ Frontend/src/markup/Pages/MakeOffer/components/Services.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { axiosPrivate } from "../../../../utils/axios"
 import url from "../../../../utils/baseUrl"
+import ClipLoader from "react-spinners/ClipLoader"
 
 import ServiceCard from "./ServiceCard"
 // var bnr = require("./../../images/banner/bnr1.jpg");
@@ -18,19 +19,23 @@ const postBox = [
 
 function Services({ id }) {
   const [services, setServices] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
   const allData = () => {
     // axios({
     // 	method: 'GET',
     // 	url: `${baseURL}api/v1/account/professional-services/`,
 
     // })
+    setLoading(true)
     axiosPrivate
       .get(`${url.baseURL}api/v1/account/professional-services/?user_id=${id}`)
       .then((res) => {
         console.log(res.data.results)
         setServices(res.data.results)
+        setLoading(false)
       })
       .catch((e) => {
+        setLoading(false)
         if (e.response?.status === 400) {
           console.log(e?.response?.data?.non_field_errors[0])
         } else {
@@ -45,12 +50,23 @@ function Services({ id }) {
   return (
     <div>
       <ul className="row">
-        {services.length > 0 &&
+        {loading ? (
+          <div className="loader">
+            <ClipLoader color={"#2e55fa"} loading={true} size={150} />
+          </div>
+        ) : services.length > 0 ? (
           services?.map((item, index) => (
             <div className="col-lg-4 col-sm-12 col-12 m-b20">
               <ServiceCard item={item} key={index} />
             </div>
-          ))}
+          ))
+        ) : (
+          <div className="col-lg-12 col-sm-12 col-12 m-b20">
+            <div className="alert alert-secondary" role="alert">
+              No Services Found
+            </div>
+          </div>
+        )}
       </ul>
     </div>
   )
