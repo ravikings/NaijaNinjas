11143c404faacdc941e038c33b70227cf9c4c71f
ravikings <39674414+ravikings@users.noreply.github.com>
2022-09-06 16:33:19 -0500

    add search to chat

--- server/accounts/serializers.py
+++ server/accounts/serializers.py
@@ -31,7 +31,8 @@ from accounts.models import (
     PublicQuotes,
     IpModel,
 )
-#from .models import IpModel, RunnerProfile, Review
+
+# from .models import IpModel, RunnerProfile, Review
 from .utilis import send_verify_email
 import arrow
 
@@ -55,11 +56,11 @@ class CustomRegisterSerializer(RegisterSerializer):
             user.phone_number = self.data.get("phone_number")
             user.is_a_runner = self.data.get("is_a_runner")
             email = self.data.get("email")
-            user.save()   
+            user.save()
         except IntegrityError as e:
 
             raise e("error: sorry phone number already exist")
- 
+
         current_site = get_current_site(request)
         user = AccountUser.objects.get(email=str(email))
         uid = urlsafe_base64_encode(force_bytes(user.pk))
@@ -69,8 +70,9 @@ class CustomRegisterSerializer(RegisterSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    
+
     last_seen_time = serializers.SerializerMethodField()
+
     class Meta:
         model = AccountUser
         fields = ["id", "username", "last_seen_time"]
@@ -78,17 +80,23 @@ class UserSerializer(serializers.ModelSerializer):
     def get_last_seen_time(self, instance):
 
         dateTimeObj = instance.last_seen
-        #timestampStr = dateTimeObj.strftime("%b-%d-%Y %I:%M%p")
         current_time = timezone.now()
-        time_difference = current_time - dateTimeObj
-        print("currentr time")
-        print(time_difference)
-        return 2 #datetime.timedelta(seconds = now_aware.)
+        time = current_time - dateTimeObj
+        day = time.total_seconds() // (24 * 3600)
+        time = time.total_seconds() % (24 * 3600)
+        hour = time // 3600
+        time %= 3600
+        minutes = time // 60
+        time %= 60
+        seconds = time
+        return {"days": day, "hour": hour, "minutes": minutes, "seconds": seconds}
+
 
 class ContractUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccountUser
         fields = ["email"]
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
@@ -99,6 +107,7 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = RunnerProfile
         fields = "__all__"
 
+
 class PrivateProfileSerializer(serializers.ModelSerializer):
     """
     Profile serializers use profile for picture uploads and retrieve
@@ -107,6 +116,7 @@ class PrivateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = RunnerProfile
         fields = "__all__"
+
 
 class PublicProfileSerializer(serializers.ModelSerializer):
     """
@@ -124,22 +134,35 @@ class PublicProfileSerializer(serializers.ModelSerializer):
         data.pop("user_set_status")
         return data
 
+
 class BiddersProfileSerializer(serializers.ModelSerializer):
     """
     Profile serializers use profile for picture uploads and retrieve
     """
 
     photo = serializers.ImageField()
+
     class Meta:
         model = RunnerProfile
-        fields = ("first_name","last_name", "photo", "status")
+        fields = ("first_name", "last_name", "photo", "status")
+
+
 class UserResumeSerializer(serializers.ModelSerializer):
     """
     resume search serializers use for entry data for resume from ui
     """
+
     class Meta:
         model = RunnerResume
         fields = "__all__"
+
+
+class ChatSearchProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RunnerProfile
+        fields = ("author", "first_name", "last_name", "title", "photo", "status")
+
 
 class ProfileSerializerWithResume(serializers.ModelSerializer):
     """
@@ -147,13 +170,14 @@ class ProfileSerializerWithResume(serializers.ModelSerializer):
     """
 
     user_profile = UserResumeSerializer(read_only=True, many=True)
-    #a_runner = serializers.SerializerMethodField()
+    # a_runner = serializers.SerializerMethodField()
     class Meta:
         model = RunnerProfile
         exclude = ("address", "login_tracker", "user_set_status")
         read_only_fields = ("status",)
+
     # def get_a_runner(self, instance):
-    
+
     #     return AccountUser.objects.filter(id=instance.author_id).values("is_a_runner")
 
     # def to_representation(self, instance):
@@ -165,10 +189,12 @@ class ProfileSerializerWithResume(serializers.ModelSerializer):
     #     #     return None
     #     return data
 
+
 class PhotosSerializer(serializers.ModelSerializer):
     """
     Photo serializers use profile for picture uploads and retrieve
     """
+
     class Meta:
         model = Photo
         fields = "__all__"
@@ -208,6 +234,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         model = Review
         fields = "__all__"
 
+
 class ClientReviewSerializer(serializers.ModelSerializer):
     """
     Review serializers use profile for picture uploads and retrieve
@@ -216,6 +243,7 @@ class ClientReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClientReview
         fields = "__all__"
+
 
 class RunnerProfileSerializer(serializers.ModelSerializer):
     """
@@ -226,11 +254,10 @@ class RunnerProfileSerializer(serializers.ModelSerializer):
         model = RunnerProfile
         fields = "__all__"
         exclude = ("login_tracker", "user_set_status")
-        read_only_fields = ("status")
+        read_only_fields = "status"
 
 
 class UserAccountSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = AccountUser
         fields = ("id", "email", "phone_number", "is_a_runner")
@@ -238,15 +265,14 @@ class UserAccountSerializer(serializers.ModelSerializer):
 
 
 class UserProfileSearchSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = RunnerProfile
         exclude = ("address",)
 
 
 class ServiceSerializer(serializers.ModelSerializer):
-    
-    #service_image = serializers.SerializerMethodField()
+
+    # service_image = serializers.SerializerMethodField()
 
     class Meta:
         model = Service
@@ -256,27 +282,30 @@ class ServiceSerializer(serializers.ModelSerializer):
 
     #     return instance.image.url
 
-class ProjectPhotoSerializer(serializers.ModelSerializer):
 
+class ProjectPhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProjectPhoto
         fields = "__all__"
 
+
 class ProjectsSerializer(serializers.ModelSerializer):
-    
+
     photos = serializers.SerializerMethodField()
+
     class Meta:
         model = Projects
         fields = "__all__"
 
     def get_photos(self, instance):
 
-        query =  ProjectPhoto.objects.filter(project=instance.id)
+        query = ProjectPhoto.objects.filter(project=instance.id)
         serializer = ProjectPhotoSerializer(query, many=True)
         return serializer.data
 
+
 # class UserOnlineSerializer(serializers.ModelSerializer):
-    
+
 #     online_status = serializers.SerializerMethodField()
 #     class Meta:
 #         model = RunnerProfile
@@ -304,7 +333,8 @@ class ProjectsSerializer(serializers.ModelSerializer):
 #             except:
 
 #                 return "offline"
-        
+
+
 class UserSearchDetialSerializer(serializers.ModelSerializer):
 
     total_reviews = serializers.SerializerMethodField()
@@ -320,7 +350,7 @@ class UserSearchDetialSerializer(serializers.ModelSerializer):
             "photo",
             "salary",
             "total_reviews",
-            "total_views", 
+            "total_views",
         )
 
     def get_total_views(self, instance):
@@ -337,22 +367,19 @@ class ResetPasswordEmailRequestSerializer(serializers.Serializer):
     redirect_url = serializers.CharField(max_length=500, required=False)
 
     class Meta:
-        fields = ['email']
+        fields = ["email"]
+
 
 class SetNewPasswordSerializer(serializers.Serializer):
-    password1 = serializers.CharField(
-        min_length=6, max_length=68)
-    password2 = serializers.CharField(
-        min_length=6, max_length=68)
-    token = serializers.CharField(
-        min_length=15)
+    password1 = serializers.CharField(min_length=6, max_length=68)
+    password2 = serializers.CharField(min_length=6, max_length=68)
+    token = serializers.CharField(min_length=15)
+
     class Meta:
-        fields = ['password1','password2', 'token']
+        fields = ["password1", "password2", "token"]
 
 
 class PublicQuotesSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = PublicQuotes
         fields = "__all__"
-
